e0a2f485218b0afb72dc043beae3b48b98e5a3ec
cagatayiba <94626282+cagatayiba@users.noreply.github.com>
2022-05-13 23:17:57 +0300

    last update

--- src/main/java/com/G01/onlineFishAuction/business/AuctionManager.java
+++ src/main/java/com/G01/onlineFishAuction/business/AuctionManager.java
@@ -3,9 +3,11 @@ package com.G01.onlineFishAuction.business;
 import com.G01.onlineFishAuction.dataAccess.IAuctionRepository;
 import com.G01.onlineFishAuction.entities.Auction;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class AuctionManager implements IAuctionService {
 
 
